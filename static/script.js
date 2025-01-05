876b4e5e7564fe137b5a76a72a9a118d209caf77
arjunkolani <10akolani@brightoncollege.net>
2025-01-05 10:00:52 +0000

    Tested out improvements to predictions and added wait time message

--- static/script.js
+++ static/script.js
@@ -2,9 +2,11 @@ let uploadButton = document.getElementById("uploadImage");
 let chosenImage = document.getElementById("chosen-image");
 let fileName = document.getElementById("file-name");
 
-let segmentedImage = document.getElementById("segmented-image")
+let segmentedImage = document.getElementById("segmented-image");
 let convertButton = document.getElementById("convertButton");
 
+let dots = document.getElementsByTagName("span");
+let waitMessage = document.getElementById("waitMessage");
 
 uploadButton.onchange = () => {
     let reader = new FileReader();
@@ -13,9 +15,19 @@ uploadButton.onchange = () => {
         chosenImage.setAttribute("src", reader.result);
         segmentedImage.style.display = "none";
     }
-    fileName.textContent = uploadButton.files[0].name;
+    fileName.textContent = uploadButton.files[0].name
+
+
 }
 
-convertButton.onchange = () => {
-    segmentedImage.style.display = "block";
+
+function loading(){
+    let dot_count = dots.length;
+    for (let i = 0; i < dot_count; i++){
+        dots[i].style.animation = "loading 1.8s ease-in-out infinite";
+        dots[i].style.animationDelay = String(0.2 * i).concat("s");
+    }
+
+    waitMessage.innerHTML = "Conversion will finish in around 5 seconds"
+
 }
