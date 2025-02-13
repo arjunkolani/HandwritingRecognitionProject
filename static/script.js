608beb8f77810c5e64482560def7b1dd340ecfee
arjunkolani <10akolani@brightoncollege.net>
2025-02-13 16:30:47 +0000

    Prevented errors and improved validation

--- static/script.js
+++ static/script.js
@@ -2,18 +2,22 @@ let uploadButton = document.getElementById("uploadImage");
 let chosenImage = document.getElementById("chosen-image");
 let fileName = document.getElementById("file-name");
 
-let segmentedImage = document.getElementById("segmented-image");
+//let segmentedImage = document.getElementById("segmented-image");
+let segmentedImageFigure = document.getElementById("segmented-image-figure");
 let convertButton = document.getElementById("convertButton");
 
 let dots = document.getElementsByTagName("span");
 let waitMessage = document.getElementById("waitMessage");
+
+let outputText = document.getElementById("output-text");
 
 uploadButton.onchange = () => {
     let reader = new FileReader();
     reader.readAsDataURL(uploadButton.files[0]);
     reader.onload = () => {
         chosenImage.setAttribute("src", reader.result);
-        segmentedImage.style.display = "none";
+        //segmentedImage.style.display = "none";
+        segmentedImageFigure.style.display = "none";
     }
     fileName.textContent = uploadButton.files[0].name
 
@@ -22,6 +26,8 @@ uploadButton.onchange = () => {
 
 
 function loading(){
+    outputText.innerHTML = "";
+    //convertButton.disabled = true;
     let dot_count = dots.length;
     for (let i = 0; i < dot_count; i++){
         dots[i].style.animation = "loading 1.8s ease-in-out infinite";
